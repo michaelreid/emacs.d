f6a54cf55b0b3aacaee952a449b2f0ccba0648b2
m <m@m.local>
2015-03-15 20:43:43 +1030

    Changes to init.el and some new packages installed.

--- init.el
+++ init.el
@@ -11,10 +11,6 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
-;; (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-;;                          ("marmalade" . "http://marmalade-repo.org/packages/")
-;;                          ("melpa" . "http://melpa-stable.milkbox.net/packages/")))
-
 
 ;; Load and activate emacs packages. Do this first so that the
 ;; packages are loaded before you start trying to modify them.
@@ -31,3 +27,26 @@
 ;; manually with M-x package-install
 ;; Add in your own as you wish:
 (load-file "~/.emacs.d/emacs-for-python/epy-init.el")
+
+
+
+
+
+;; Customisations for Copy & Paste
+;; And ability to use system clipboard
+ '(delete-selection-mode t)
+ '(kill-do-not-save-duplicates t)
+ '(save-interprogram-paste-before-kill t)
+ '(yank-pop-change-selection t)
+
+;; enable clipboard in emacs
+ (setq x-select-enable-clipboard t)
+
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
